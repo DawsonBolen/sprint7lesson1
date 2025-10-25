0e4a544f4fd9b22db87156190b23d89228f4100c
Dawson Bolen <dawson120@live.com>
2025-10-24 17:50:53 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const url = 'https://jsonfakery.com/games/infinite-scroll?page=1';
+const games = document.getElementById('games')
+
+fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+        return response.json();
+
+    })
+    .then(data => {
+        console.log(data)
+        data.forEach(game => {
+            let gameCard = document.createElement('div');
+            gameCard.classList.add('game-card')
+            gameCard.classList.add('card');
+
+            gameCard.innerHTML = `
+
+<div class='gc-img'>
+  <img src="${game.background_image}" class="card-img-top" alt="...">
+  </div
+  <div class="card-body gc-body p-3">
+    <h5 class="card-title">${game.name}</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+
+`
+
+            games.appendChild(gameCard)
+        })
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
+//     <div class='game-card-inner'>
+// <div class='gc-img'>
+// <img src='${game.background_image}'>
+// </div>
+// <h3>${game.name}</h3>
+// </div>
